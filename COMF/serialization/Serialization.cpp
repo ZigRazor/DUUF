3700a3108842c73ac10550270a3c9751829e3f42
ZigRazor <zigrazor@gmail.com>
2020-06-05 16:09:09 +0200

    Avoid Code Repetition

--- COMF/serialization/Serialization.cpp
+++ COMF/serialization/Serialization.cpp
@@ -19,70 +19,70 @@
 namespace DUUF {
 namespace COMF {
 
-unsigned int Serialization::serialize( const int& source, std::ostream& dest ) {
+template<typename T>
+unsigned int deserialize_template( std::istream& source, T& dest ) {
+    source.read(reinterpret_cast<char*>(&dest), sizeof(dest));
+    return sizeof(dest);
+}
+template<typename T>
+unsigned int serialize_template( const T& source, std::ostream& dest ) {
     dest.write(reinterpret_cast<const char*>(&source), sizeof(source));
     return sizeof(source);
+}
+
+unsigned int Serialization::serialize( const int& source, std::ostream& dest ) {
+    return serialize_template(source, dest);
 }
 
 unsigned int Serialization::serialize( const unsigned int& source, std::ostream& dest ) {
-    dest.write(reinterpret_cast<const char*>(&source), sizeof(source));
-    return sizeof(source);
+    return serialize_template(source, dest);
 }
 
 unsigned int Serialization::serialize( const short& source, std::ostream& dest ) {
-    dest.write(reinterpret_cast<const char*>(&source), sizeof(source));
-    return sizeof(source);
+    return serialize_template(source, dest);
 }
 
 unsigned int Serialization::serialize( const unsigned short& source, std::ostream& dest ) {
-    dest.write(reinterpret_cast<const char*>(&source), sizeof(source));
-    return sizeof(source);
+    return serialize_template(source, dest);
 }
 
 unsigned int Serialization::serialize( const long& source, std::ostream& dest ) {
-    dest.write(reinterpret_cast<const char*>(&source), sizeof(source));
-    return sizeof(source);
+    return serialize_template(source, dest);
 }
 
 unsigned int Serialization::serialize( const unsigned long& source, std::ostream& dest ) {
-    dest.write(reinterpret_cast<const char*>(&source), sizeof(source));
-    return sizeof(source);
+    return serialize_template(source, dest);
 }
 
 unsigned int Serialization::serialize( const char& source, std::ostream& dest ) {
-    dest.write(reinterpret_cast<const char*>(&source), sizeof(source));
-    return sizeof(source);
+    return serialize_template(source, dest);
 }
 
 unsigned int Serialization::serialize( const unsigned char& source, std::ostream& dest ) {
-    dest.write(reinterpret_cast<const char*>(&source), sizeof(source));
-    return sizeof(source);
+    return serialize_template(source, dest);
 }
 
 unsigned int Serialization::serialize( const float& source, std::ostream& dest ) {
-    dest.write(reinterpret_cast<const char*>(&source), sizeof(source));
-    return sizeof(source);
+    return serialize_template(source, dest);
 }
 
 unsigned int Serialization::serialize( const double& source, std::ostream& dest ) {
-    dest.write(reinterpret_cast<const char*>(&source), sizeof(source));
-    return sizeof(source);
+    return serialize_template(source, dest);
 }
 
 unsigned int Serialization::serialize( const bool& source, std::ostream& dest ) {
-    dest.write(reinterpret_cast<const char*>(&source), sizeof(source));
-    return sizeof(source);
+    return serialize_template(source, dest);
 }
 
 unsigned int Serialization::serialize( const std::string& source, std::ostream& dest ) {
     unsigned int stringLen = source.length();
-    dest.write(reinterpret_cast<const char*>(&stringLen), sizeof(stringLen));
+    serialize_template(stringLen, dest);
     dest.write(source.c_str(), stringLen);
     return sizeof(stringLen) + stringLen;
 }
 
 unsigned int Serialization::serialize( const char* source, unsigned int sizeOfSource, std::ostream& dest ) {
-    dest.write(reinterpret_cast<const char*>(&sizeOfSource), sizeof(sizeOfSource));
+    serialize_template(sizeOfSource, dest);
     dest.write(source, sizeOfSource);
     return sizeof(sizeOfSource) + sizeOfSource;
 }
@@ -92,117 +92,54 @@ unsigned int Serialization::serialize( const Serializable& source, std::ostream&
 }
 
 unsigned int Serialization::deserialize( std::istream& source, int& dest ) {
-    source.read(reinterpret_cast<char*>(&dest), sizeof(dest));
-    if ( !source ) {
-        //ERROR
-        return 0;
-    }
-    return sizeof(dest);
+    return deserialize_template(source, dest);
 }
 
 unsigned int Serialization::deserialize( std::istream& source, unsigned int& dest ) {
-    source.read(reinterpret_cast<char*>(&dest), sizeof(dest));
-    if ( !source ) {
-        //ERROR
-        return 0;
-    }
-    return sizeof(dest);
+    return deserialize_template(source, dest);
 }
 
 unsigned int Serialization::deserialize( std::istream& source, short& dest ) {
-    source.read(reinterpret_cast<char*>(&dest), sizeof(dest));
-    if ( !source ) {
-        //ERROR
-        return 0;
-    }
-    return sizeof(dest);
+    return deserialize_template(source, dest);
 }
 
 unsigned int Serialization::deserialize( std::istream& source, unsigned short& dest ) {
-    source.read(reinterpret_cast<char*>(&dest), sizeof(dest));
-    if ( !source ) {
-        //ERROR
-        return 0;
-    }
-    return sizeof(dest);
+    return deserialize_template(source, dest);
 }
 
 unsigned int Serialization::deserialize( std::istream& source, long& dest ) {
-    source.read(reinterpret_cast<char*>(&dest), sizeof(dest));
-    if ( !source ) {
-        //ERROR
-        return 0;
-    }
-    return sizeof(dest);
+    return deserialize_template(source, dest);
 }
 
 unsigned int Serialization::deserialize( std::istream& source, unsigned long& dest ) {
-    source.read(reinterpret_cast<char*>(&dest), sizeof(dest));
-    if ( !source ) {
-        //ERROR
-        return 0;
-    }
-    return sizeof(dest);
+    return deserialize_template(source, dest);
 }
 
 unsigned int Serialization::deserialize( std::istream& source, char& dest ) {
-    source.read(reinterpret_cast<char*>(&dest), sizeof(dest));
-    if ( !source ) {
-        //ERROR
-        return 0;
-    }
-    return sizeof(dest);
+    return deserialize_template(source, dest);
 }
 
 unsigned int Serialization::deserialize( std::istream& source, unsigned char& dest ) {
-    source.read(reinterpret_cast<char*>(&dest), sizeof(dest));
-    if ( !source ) {
-        //ERROR
-        return 0;
-    }
-    return sizeof(dest);
+    return deserialize_template(source, dest);
 }
 
 unsigned int Serialization::deserialize( std::istream& source, float& dest ) {
-    source.read(reinterpret_cast<char*>(&dest), sizeof(dest));
-    if ( !source ) {
-        //ERROR
-        return 0;
-    }
-    return sizeof(dest);
+    return deserialize_template(source, dest);
 }
 
 unsigned int Serialization::deserialize( std::istream& source, double& dest ) {
-    source.read(reinterpret_cast<char*>(&dest), sizeof(dest));
-    if ( !source ) {
-        //ERROR
-        return 0;
-    }
-    return sizeof(dest);
+    return deserialize_template(source, dest);
 }
 
 unsigned int Serialization::deserialize( std::istream& source, bool& dest ) {
-    source.read(reinterpret_cast<char*>(&dest), sizeof(dest));
-    if ( !source ) {
-        //ERROR
-        return 0;
-    }
-    return sizeof(dest);
+    return deserialize_template(source, dest);
 }
 
 unsigned int Serialization::deserialize( std::istream& source, std::string& dest ) {
     unsigned int stringlen = 0;
-    source.read(reinterpret_cast<char*>(&stringlen), sizeof(stringlen));
-    if ( !source ) {
-        //ERROR
-        return 0;
-    }
+    deserialize_template(source, stringlen);
     char dest_c[stringlen + 1];
     source.read(dest_c, stringlen);
-    if ( !source ) {
-        //ERROR
-        return 0;
-    }
     dest_c[stringlen] = '\0';
     dest = dest_c;
     return sizeof(stringlen) + stringlen;
@@ -210,17 +147,9 @@ unsigned int Serialization::deserialize( std::istream& source, std::string& dest
 
 unsigned int Serialization::deserialize( std::istream& source, unsigned int sizeOfDestination, char* dest ) {
     unsigned int stringlen = 0;
-    source.read(reinterpret_cast<char*>(&stringlen), sizeof(stringlen));
-    if ( !source ) {
-        //ERROR
-        return 0;
-    }
+    deserialize_template(source, stringlen);
     char dest_c[stringlen];
     source.read(dest_c, stringlen);
-    if ( !source ) {
-        //ERROR
-        return 0;
-    }
     if ( stringlen <= sizeOfDestination ) {
         std::memcpy(dest, dest_c, stringlen);
     } else {
