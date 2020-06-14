c5a98715d94fd65d9cffb54cb3618461b9f0f02c
ZigRazor <zigrazor@gmail.com>
2020-06-14 21:17:23 +0200

    Introduced test on send

--- TESTS/test_udp_sender.cpp
+++ TESTS/test_udp_sender.cpp
@@ -2,13 +2,29 @@
 #include "../COMF/udp_sender_receiver/UdpSender.h"
 
 TEST(test_udp_sender, test1) {
-    DUUF::COMF::UDP::UdpSender* mySender = nullptr;
-    mySender = new DUUF::COMF::UDP::UdpSender("127.0.0.1", 20000);
+	DUUF::COMF::UDP::UdpSender *mySender = nullptr;
+	mySender = new DUUF::COMF::UDP::UdpSender("127.0.0.1", 20000);
 
-    EXPECT_TRUE(mySender != nullptr);
+	EXPECT_TRUE(mySender != nullptr);
 
-    delete (mySender);
+	delete (mySender);
 
-    EXPECT_TRUE(mySender == nullptr);
+	EXPECT_TRUE(mySender == nullptr);
 }
 
+TEST(test_udp_sender, test2) {
+	DUUF::COMF::UDP::UdpSender *mySender = nullptr;
+	mySender = new DUUF::COMF::UDP::UdpSender("127.0.0.1", 20000);
+
+	EXPECT_TRUE(mySender != nullptr);
+
+	std::string msg = "test_send";
+
+	size_t byte_sent = mySender->send(msg.c_str(), msg.length());
+
+	EXPECT_EQ(byte_sent,msg.length());
+
+	delete (mySender);
+
+	EXPECT_TRUE(mySender == nullptr);
+}
